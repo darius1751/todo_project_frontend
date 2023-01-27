c94877bb3fa2612c53c59ae89016d01922e24444
darius1751 <lupequi12@gmail.com>
2023-01-27 15:39:34 -0500

    Modifications to create-task page

--- src/pages/CreateTask.tsx
+++ src/pages/CreateTask.tsx
@@ -1,23 +1,31 @@
 import { DashboardNav } from "../components/DashboardNav"
 import { useForm } from "../hooks/useForm";
 import { Task } from "../models";
+import { FormEvent } from "react";
 import '../styles/create-task.css';
+import { Footer } from "../components/Footer";
 const initialTask:Task = {
     name:'',
     description:''
 }
 export const CreateTask = () => {
     const [newTask, handleChange] = useForm<Task>(initialTask);
+    const handleSubmit = (e:FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(`Create task`);
+    }
     return (
         <div className ="create-task dashboard-page">
             <DashboardNav/>
-            <form>
-                <input type="text" placeholder="Ingrese el titulo de la tarea" name='name' value = {newTask.name} onChange={handleChange} autoComplete={'off'}/>
+            <h2>Crea tu tarea :D</h2>
+            <form onSubmit={handleSubmit} className='form-create-task'>
+                <input type="text" placeholder="Ingrese el titulo de la tarea" name='name' value = {newTask.name} onChange={handleChange} autoComplete={'off'} className='form-input-text'/>
                 <br />
-                <input type="text" placeholder="Ingrese la descripcion de la tarea" name='description' value={newTask.description}  onChange={handleChange} autoComplete={'off'}/>
+                <input type="text" placeholder="Ingrese la descripcion de la tarea" name='description' value={newTask.description}  onChange={handleChange} autoComplete={'off'} className='form-input-text'/>
                 <br />
                 <input type="submit" value='Crear tarea'/>
             </form>
+            <Footer/>
         </div>
     )
 }
